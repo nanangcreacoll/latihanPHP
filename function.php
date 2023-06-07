3ed82c789730f31767592441fc4af61785fd166e
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-06-07 18:38:37 +0700

    first commit

---
+++ function.php
@@ -0,0 +1,115 @@
+<?php
+$connect = mysqli_connect("localhost","root","","latihanphp");
+
+function query($query) {
+    global $connect;
+    $result = mysqli_query($connect,$query);
+    $rows = [];
+    while($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data){
+    global $connect;
+    
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+
+    $gambar = upload();
+    if(!$gambar){
+        return false;
+    }
+
+    $query = "INSERT INTO mahasiswa VALUES ('','$nama','$nrp','$email','$jurusan','$gambar')";
+
+    mysqli_query($connect, $query);
+
+    return mysqli_affected_rows($connect);
+}
+
+function upload()
+{
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+    if($error === 4){
+        echo "
+        <script>
+            alert('pilih gambar terlebih dahulu!');
+        </script>
+        ";
+        return false;
+    }
+
+    $ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+    $ekstensiGambar = explode('.', $namaFile);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+    if( !in_array($ekstensiGambar, $ekstensiGambarValid)){
+        echo "
+        <script>
+            alert('yang anda upload bukan gambar!');
+        </script>
+        ";
+        return false;
+    }
+    if ($ukuranFile>(5*1048576)){
+        echo "
+        <script>
+            alert('ukuran gambar terlalu besar!');
+        </script>
+        ";
+        return false;
+    }
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $ekstensiGambar;
+    move_uploaded_file($tmpName,'img/'.$namaFileBaru);
+    return $namaFileBaru;
+}
+
+function hapus($id){
+    global $connect;
+
+    mysqli_query($connect, "DELETE FROM mahasiswa WHERE id = $id");
+
+    return mysqli_affected_rows($connect);
+}
+
+function ubah($data){
+    global $connect;
+    
+    $id = $data["id"];
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambarLama = htmlspecialchars($data["gambarLama"]);
+    if($_FILES['gambar']['error'] === 4){
+        $gambar = $gambarLama;
+    }else{
+        $gambar = upload();
+        if(upload()){
+            unlink("img/"."$gambarLama");
+        }
+    }
+
+    $query = "UPDATE mahasiswa SET nrp = '$nrp', nama = '$nama', email = '$email', jurusan = '$jurusan', gambar = '$gambar' WHERE id = $id";
+
+    mysqli_query($connect, $query);
+
+    return mysqli_affected_rows($connect);
+}
+
+function cari($keyword)
+{
+    $query = "SELECT * FROM mahasiswa WHERE nama LIKE '%$keyword%' OR nrp LIKE '%$keyword%' OR email LIKE '%$keyword%' OR jurusan LIKE '%$keyword%'";
+    return query($query);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+require 'function.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+if(isset($_POST["cari"])){
+    $mahasiswa = cari($_POST["keyword"]);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+        <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian ..." autocomplete="off">
+        <button type="submit" name="cari">Cari!</button>
+    </form>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <td>No.</td>
+            <td>Aksi</td>
+            <td>Gambar</td>
+            <td>Nama</td>
+            <td>NRP</td>
+            <td>Email</td>
+            <td>Jurusan</td>
+        </tr>
+
+        <?php $i = 1;?>
+        <?php foreach( $mahasiswa as $mhs ): ?>
+        <tr>
+            <td><?= $i?></td>
+            <td>
+                <a href="ubah.php?id=<?= $mhs["id"];?>">ubah</a>
+                <a href="hapus.php?id=<?= $mhs["id"];?>" onclick = "return confirm ('yakin?');">hapus</a>
+            </td>
+            <td><img src="img/<?= $mhs["gambar"];?>" width="100"></td>
+            <td><?= $mhs["nama"];?></td>
+            <td><?= $mhs["nrp"];?></td>
+            <td><?= $mhs["email"];?></td>
+            <td><?= $mhs["jurusan"];?></td>
+        </tr>
+        <?php $i++; ?>
+        <?php endforeach;?>
+    </table>
+</body>
+</html>
