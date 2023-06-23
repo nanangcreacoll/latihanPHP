334587778bec7b11031db56902a77279d15f46ca
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-06-23 18:07:53 +0700

    update ajax

--- index.php
+++ index.php
@@ -31,37 +31,42 @@ if(isset($_POST["cari"])){
     <br><br>
 
     <form action="" method="post">
-        <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian ..." autocomplete="off">
-        <button type="submit" name="cari">Cari!</button>
+        <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian ..." autocomplete="off" id="keyword">
+        <button type="submit" name="cari" id="tombol-cari">Cari!</button>
     </form>
 
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <td>No.</td>
-            <td>Aksi</td>
-            <td>Gambar</td>
-            <td>Nama</td>
-            <td>NRP</td>
-            <td>Email</td>
-            <td>Jurusan</td>
-        </tr>
+    <br>
 
-        <?php $i = 1;?>
-        <?php foreach( $mahasiswa as $mhs ): ?>
-        <tr>
-            <td><?= $i?></td>
-            <td>
-                <a href="ubah.php?id=<?= $mhs["id"];?>">ubah</a>
-                <a href="hapus.php?id=<?= $mhs["id"];?>" onclick = "return confirm ('yakin?');">hapus</a>
-            </td>
-            <td><img src="img/<?= $mhs["gambar"];?>" width="100"></td>
-            <td><?= $mhs["nama"];?></td>
-            <td><?= $mhs["nrp"];?></td>
-            <td><?= $mhs["email"];?></td>
-            <td><?= $mhs["jurusan"];?></td>
-        </tr>
-        <?php $i++; ?>
-        <?php endforeach;?>
-    </table>
+    <div id="container">
+        <table border="1" cellpadding="10" cellspacing="0">
+            <tr>
+                <td>No.</td>
+                <td>Aksi</td>
+                <td>Gambar</td>
+                <td>Nama</td>
+                <td>NRP</td>
+                <td>Email</td>
+                <td>Jurusan</td>
+            </tr>
+
+            <?php $i = 1;?>
+            <?php foreach( $mahasiswa as $mhs ): ?>
+            <tr>
+                <td><?= $i?></td>
+                <td>
+                    <a href="ubah.php?id=<?= $mhs["id"];?>">ubah</a>
+                    <a href="hapus.php?id=<?= $mhs["id"];?>" onclick = "return confirm ('yakin?');">hapus</a>
+                </td>
+                <td><img src="img/<?= $mhs["gambar"];?>" width="100"></td>
+                <td><?= $mhs["nama"];?></td>
+                <td><?= $mhs["nrp"];?></td>
+                <td><?= $mhs["email"];?></td>
+                <td><?= $mhs["jurusan"];?></td>
+            </tr>
+            <?php $i++; ?>
+            <?php endforeach;?>
+        </table>
+    </div>
+    <script src="js/script.js"></script>
 </body>
 </html>
