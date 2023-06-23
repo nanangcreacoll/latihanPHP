dd1e866b35020c766a42d826015cdd61506ea708
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-06-23 18:17:56 +0700

    update jquery

--- index.php
+++ index.php
@@ -74,6 +74,7 @@ if(isset($_POST["cari"])){
             <?php endforeach;?>
         </table>
     </div>
+    <script src="js/jquery-3.7.0.min.js"></script>
     <script src="js/script.js"></script>
 </body>
 </html>
